d7393961a8832b43bb6a7876a63d1fdfe633d4c0
Chiara Ripanti <chiararipanti@MacBook-Air-di-Chiara.local>
2019-05-26 17:06:09 +0100

    added proper auth

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ const TaskRouter = require("./routers/task")
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use((req,res,next) => {
-    if(req.method === "GET"){
-        res.send("Get requests are disbled")
-    }
-    next()
-})
+// app.use((req,res,next) => {
+//     if(req.method === "GET"){
+//         res.send("Get requests are disbled")
+//     }
+//     next()
+// })
 
 app.use(express.json())
 app.use(UserRouter)
@@ -21,16 +21,3 @@ app.use(TaskRouter)
 app.listen(port, () => {
     console.log("Server is listening in port " + port)
 })
-
-const jwt = require("jsonwebtoken")
-
-const myFunction = async() => {
-    const token = jwt.sign({_id: "ajfsoj"}, "thisisatest", {expiresIn : "7 days"})
-    console.log(token)
-
-    const verify = jwt.verify(token, "thisisatest")
-    console.log(verify)
-
-}
-
-//myFunction()
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,15 +1,50 @@
 const express = require("express")
 const router = new express.Router()
-
+const auth = require("../middleware/auth")
 const User = require("../models/user")
 
-router.get("/users", async (req, res) => {
+router.post("/users/login", async (req, res) => {
     try{
-        const users = await User.find({})
-        res.send(users)
+        const user = await User.findByUserCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        user.tokens = user.tokens.concat({token})
+        await user.save()
+        res.send({user, token})
+    }catch(error){
+        res.status(400).send()
+    }
+})
+
+router.post("/users/logout", auth, async (req, res) => {
+    try{
+        req.user.tokens = req.user.tokens.filter((token) => {
+            token.token != req.token
+        })
+
+        await req.user.save()
+        res.send()
+
     }catch(error){
         res.status(500).send()
     }
+
+})
+
+router.post("/users/logoutall", auth, async (req, res) => {
+    try{
+        req.user.tokens = []
+
+        await req.user.save()
+        res.send()
+
+    }catch(error){
+        res.status(500).send()
+    }
+
+})
+
+router.get("/users/me", auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get("/users/:id", async (req, res) => {
@@ -95,18 +130,6 @@ router.delete("/users/:id", async (req, res) => {
         res.send(user)
     }catch(error){
         res.status(400).send(error)
-    }
-})
-
-router.post("/users/login", async (req, res) => {
-    try{
-        const user = await User.findByUserCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
-        user.tokens = user.tokens.concat({token})
-        await user.save()
-        res.send({user, token})
-    }catch(error){
-        res.status(400).send()
     }
 })
 
